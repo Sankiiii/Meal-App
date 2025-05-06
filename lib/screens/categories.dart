2be82490a49628bb1eee7acbecb0b3300d183a4a
Sankiii <sanketangane2006@gmail.com>
2025-05-06 16:52:05 +0530

    Current Working filled added

---
+++ lib/screens/categories.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/models/category.dart';
+import 'package:meal_app/screens/meals.dart';
+import 'package:meal_app/widgets/category_grid_item.dart';
+
+class CategoriesScreeen extends StatelessWidget {
+  void _selectCategory(BuildContext context , Category category) {
+    final  filterdMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: category.title, meals: filterdMeals),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pick u arr Category..")),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context , category);
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
